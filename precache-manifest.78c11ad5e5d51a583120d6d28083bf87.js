3721b041638dac4918d63771cfd37ab88ea00126
OlafurjonHI <ojv1@hi.is>
2020-06-17 22:52:04 +0000

    initial commit

---
+++ precache-manifest.78c11ad5e5d51a583120d6d28083bf87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e46f9d9a0f9f776e593b97553abbc5",
+    "url": "/React_robots_test/index.html"
+  },
+  {
+    "revision": "1339d0a23d60a9bd214b",
+    "url": "/React_robots_test/static/css/main.7a62e5af.chunk.css"
+  },
+  {
+    "revision": "7bd7ea342e0c395ad50b",
+    "url": "/React_robots_test/static/js/2.c5283f39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_robots_test/static/js/2.c5283f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1339d0a23d60a9bd214b",
+    "url": "/React_robots_test/static/js/main.2ed7f19d.chunk.js"
+  },
+  {
+    "revision": "00d1a05dc3df94c0bab1",
+    "url": "/React_robots_test/static/js/runtime-main.97f537f3.js"
+  }
+]);
